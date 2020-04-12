4adfedce634edbc156b60401014ff334da03203d
Gowry Sugathan <gowrysugathan18@gmail.com>
2020-04-12 14:48:58 +0530

    reversePrint

--- LinkedDel.java
+++ LinkedDel.java
@@ -84,3 +84,4 @@ public class LinkedDel {
     b.print();
   }
 }
+
---
+++ reversePrintLinkedList.java
@@ -0,0 +1,18 @@
+// Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+    if(head==null){
+        return;
+    }
+    reversePrint(head.next);
+    System.out.print(head.data+"\n");
+    } 
