f381c897e9bc97960627203e168f2ba12ce0cb09
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-08-05 13:19:28 +0530

    Upate deletion and heap sort

--- MaxBinaryHeap.cpp
+++ MaxBinaryHeap.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	int heap[n],k,a,temp,x;
+	int heap[n],k,a,temp,x,b,c;
 	int i=0;
 	while(i<n){
 		cin>>x;
@@ -19,7 +19,51 @@ int main(){
 		}
 	i++;
 	}
+	cout<<"After Inserion"<<endl;
 	for(int i = 0;i < n;i++){
 		cout<<heap[i]<<" ";
-	}	
+	}
+	i=0;
+	k=n-1;
+	while(k>0){
+		i=0;
+		temp=heap[i];
+		heap[i]=heap[k];
+		heap[k]=temp;
+		k--;
+		while(1){
+			a=(2*i)+1;
+			b=(2*i)+2;
+			if(a<=k && b<=k){
+				c=(heap[a]>heap[b] ? a:b);
+				if(heap[c]>heap[i]){
+					temp=heap[i];
+					heap[i]= heap[c];
+					heap[c]=temp;
+					i=c;
+				}
+				else{
+					break;
+				}
+			}
+			else if(a<=k){
+				if(heap[a]>heap[i]){
+					temp=heap[a];
+					heap[a]=heap[i];
+					heap[i]=temp;
+					i=a;
+				}
+				else{
+					break;
+				}
+			}
+			else{
+				break;
+			}
+		}
+	}
+	cout<<"\n"<<"Ater Heap Sort"<<endl;
+	for(int i = 0;i < n;i++){
+		cout<<heap[i]<<" ";
+	}
 }
